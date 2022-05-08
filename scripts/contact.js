4b5bcb190499269b82e2472f6f4e715cc1c13333
facundo <facurmedina@gmail.com>
2022-05-07 22:09:51 -0300

    test change

---
+++ scripts/contact.js
@@ -0,0 +1,18 @@
+function register(event) {
+  
+  const el = event.target.elements;
+  event.preventDefault()
+  params = {
+    from_name: el.email.value,
+    userName: el.userName.value,
+    title: '<h1>Bienvenido a Games Chief</h1>',
+    message: `Gracias por comunicarse con nosotros. Nos pondremos en contacto a la brevedad`,
+    // email: el.user_email.value,
+    // reply_to: `facurmedina@gmail.com`
+  }
+  
+  emailjs.send('service_cocsh87', 'template_jbus0xp', params)
+    .then((resp) => swal ("El formulario ha sido enviado correctamente.","En breve nos pondremos en contacto para resolver su consulta", "success"))
+    .catch((error) => swal ("Error", "No se pudo enviar el correo, por favor verifique los datos", "erorr"))
+  emailjs.send('service_cocsh87', 'template_jbus0xp', params)
+}
