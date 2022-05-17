702cabcba50834264ebd2276e0c059f4de129f16
facundo <facurmedina@gmail.com>
2022-05-16 21:00:53 -0300

    Formulario de consulta realizado

--- scripts/contact.js
+++ scripts/contact.js
@@ -3,12 +3,14 @@ function register(event) {
   const el = event.target.elements;
   event.preventDefault()
   params = {
-    from_name: el.email.value,
+    from_name: 'facurmedina@gmail.com',
     userName: el.userName.value,
-    title: '<h1>Bienvenido a Games Chief</h1>',
-    message: `Gracias por comunicarse con nosotros. Nos pondremos en contacto a la brevedad`,
+    user_email: el.user_email.value,
+    tel_number: el.tel_number.value,
+    business: el.business.value,
+    comments: el.comments.value,
+    
   }
-  
   emailjs.send('service_cocsh87', 'template_jbus0xp', params)
     .then((resp) => swal ("El mensaje ha sido enviado correctamente.","En breve nos pondremos en contacto para resolver su consulta", "success"))
     .catch((error) => swal ("Error", "No se pudo enviar el correo, por favor verifique los datos", "erorr"))
